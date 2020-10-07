e57f670fb46c5d58ec811ec468bec9e3d0d82cde
Daniel Ziegler <dmz@openai.com>
2020-10-07 11:39:07 -0700

    don't be so annoying

--- src/handler.ts
+++ src/handler.ts
@@ -4,22 +4,24 @@ import calculateAQI from './calculateAQI'
 declare const SLACK_POST_URL: string
 
 export async function handleRequest(request: Request): Promise<Response> {
-  const text = await getStatus()
-  return new Response(text)
+  const aqi = await getAqi()
+  return new Response(`AQI ${aqi}`)
 }
 
 export async function handleScheduled(event: ScheduledEvent): Promise<void> {
-  const text = await getStatus()
-  await postToSlack(text)
+  const aqi = await getAqi()
+  if (aqi <= 50) {
+    await postToSlack(`AQI ${aqi}`)
+  }
 }
 
-async function getStatus(): Promise<string> {
+async function getAqi(): Promise<number> {
   const apiUrl = "https://www.purpleair.com/json?key=14036BZ1E97OKE1B&show=23905"
   const resp = await fetch(new Request(apiUrl))
   const data = await resp.json()
   const pm25 = +data.results[0]["pm2_5_atm"]
   const aqiData = calculateAQI(pm25)
-  return `AQI: ${aqiData.AQI}`
+  return aqiData.AQI
 }
 
 async function postToSlack(text: string): Promise<void> {
