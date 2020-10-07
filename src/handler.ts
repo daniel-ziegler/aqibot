876b72df02d3ff5b14fa1b1c5b98ff9809ede503
Daniel Ziegler <dmz@openai.com>
2020-10-06 18:55:52 -0700

    .

--- src/handler.ts
+++ src/handler.ts
@@ -4,10 +4,10 @@ export async function handleRequest(request: Request): Promise<Response> {
   const apiUrl = "https://www.purpleair.com/json?key=14036BZ1E97OKE1B&show=23905"
   const resp = await fetch(new Request(apiUrl))
   const data = await resp.json()
-  const concentration = +data.results[0]["pm2_5_atm"]
-  const text = `μg/m^3: ${concentration}`
+  const pm25 = +data.results[0]["pm2_5_atm"]
+  const text = `μg/m^3: ${pm25}`
   await postToSlack(text)
-  return new Response(`tried to post: '${text}' to ${SLACK_POST_URL}`)
+  return new Response(`Posted '${text}' to ${SLACK_POST_URL}`)
 }
 
 async function postToSlack(text: string): Promise<void> {
