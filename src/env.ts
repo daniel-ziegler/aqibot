78e2813e6dbfd246ef173dce712fa4a69dbbb68d
Daniel Ziegler <dmz@openai.com>
2020-10-06 18:35:49 -0700

    it work like dis

--- src/env.ts
+++
@@ -1,1 +0,0 @@
-export declare const SLACK_POST_URL: string
--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,4 @@
-import { SLACK_POST_URL } from './env'
-// declare const SLACK_POST_URL: string
+declare const SLACK_POST_URL: string
 
 export async function handleRequest(request: Request): Promise<Response> {
   const apiUrl = "https://www.purpleair.com/json?key=14036BZ1E97OKE1B&show=23905"
@@ -7,7 +6,7 @@ export async function handleRequest(request: Request): Promise<Response> {
   const data = await resp.json()
   const concentration = +data.results[0]["pm2_5_atm"]
   const text = `μg/m^3: ${concentration}`
-  // await postToSlack(text)
+  await postToSlack(text)
   return new Response(`tried to post: '${text}' to ${SLACK_POST_URL}`)
 }
 
